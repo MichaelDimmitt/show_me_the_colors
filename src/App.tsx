11142ae7077ec0f48dc6d21fd36e5241e60bd819
michael <michaelgdimmitt@gmail.com>
2024-02-13 09:57:50 -0500

    getting started

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { listOfColors } from './colorsFromHex';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function invertColor(hex: string, bw: boolean) {
+  if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+  }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+  }
+  var r = parseInt(hex.slice(0, 2), 16),
+      g = parseInt(hex.slice(2, 4), 16),
+      b = parseInt(hex.slice(4, 6), 16);
+  if (bw) {
+      // https://stackoverflow.com/a/3943023/112731
+      return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+          ? '#000000'
+          : '#FFFFFF';
+  }
+  // invert color components
+  const red = (255 - r).toString(16);
+  const green = (255 - g).toString(16);
+  const blue = (255 - b).toString(16);
+  // pad each with zeros and return
+  return "#" + padZero(red) + padZero(green) + padZero(blue);
+}
+function padZero(str: any, len?: any) {
+  len = len || 2;
+  var zeros = new Array(len).join('0');
+  return (zeros + str).slice(-len);
 }
 
-export default App
+export const DisplaySvgs = () => {
+  return (<>
+      <div style={{  backgroundColor: 'gray', paddingTop: '4rem', paddingLeft: '4rem' }}>
+          <div className='border border-3 rounded' style={{ backgroundColor: 'whitesmoke', width: '75%', padding: "2rem" }} >
+              <h1>Color Section</h1>
+              <section>
+                  <div className='d-flex gap-4 p-2 flex-wrap' style={{justifyContent: 'center'}}>
+                      {listOfColors.map(([colorName, hexColor]: any) => {
+                          return <div className='d-flex flex-column' style={{ width: '123px' }}>
+                              <div>{colorName}</div>
+                              <div>{hexColor}</div>
+                              <div className="rounded px-2" style={{ backgroundColor: hexColor, color: invertColor(hexColor, true) }}>color</div>
+                          </div>
+                      })}
+                  </div>
+                  <br />
+                  
+              </section>
+          </div>
+      </div>
+
+  </>)
+}
+
+export default DisplaySvgs
